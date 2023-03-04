84c619e117c3f40774871128e18ecbcf813b0955
mouhamed <mouhadje@gmail.com>
2023-03-04 19:29:24 +0100

    requiredcommit

--- models/categorie.js
+++ models/categorie.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 const categorieSchema=mongoose.model('categorie',{
 title:{
     type :String,
+    required : true,
 },
 
 description:{
     type :String,
+    required : true,
 },
 image:{
     type :String,
+    required : true,
 }
 
 
